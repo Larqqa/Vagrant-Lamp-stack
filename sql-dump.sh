b941a24670136ebf1444c0c83bfef89839d24ec7
lrq <lrq@lrq.fi>
2020-06-15 09:30:34 +0300

    Cleaned up the cli tool repo

--- sql-dump.sh
+++
@@ -1,8 +0,0 @@
-# Get the Database name from the Vagrantfile
-DB=$(sed -n '/"DB" => .*/p' Vagrantfile | sed 's/^ *"DB" => //' | tr -d '",')
-
-# Make a Database dump to shared folder
-echo "-- Making database dump of $DB --" 
-vagrant ssh -c "sudo mysqldump $DB > /var/www/db_dump.sql"
-
-echo "-- Finished --"
